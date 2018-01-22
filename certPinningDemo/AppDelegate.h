87e5d6ceb552ab5b0dfbebb3a45ddbd64d7b86f8
huobb0 <zsombor.kovacs@gmail.com>
2018-01-22 15:30:20 +0100

    Initial Commit

---
+++ certPinningDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  certPinningDemo
+//
+//  Created by Zsombor on 2018. 01. 22..
+//  Copyright © 2018. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ certPinningDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  certPinningDemo
+//
+//  Created by Zsombor on 2018. 01. 22..
+//  Copyright © 2018. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
